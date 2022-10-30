03c00275c934294a24902ed2e8c83bbc4dee7e91
rafandoo <rafaelcamargo.inf@gmail.com>
2022-10-30 16:22:14 -0300

    inclusão de documentação

--- assets/js/switch.js
+++ assets/js/switch.js
@@ -5,6 +5,12 @@ var switchBtnRight = document.querySelector('.switch-button-case.right');
 var switchBtnLeft = document.querySelector('.switch-button-case.left');
 var activeSwitch = document.querySelector('.active');
 
+/**
+ * When the switchLeft function is called, the switchBtnRight class is removed from the switchBtnRight
+ * element, the switchBtnLeft class is added to the switchBtnLeft element, the left property of the
+ * activeSwitch element is set to 0%, and the borderRadius property of the activeSwitch element is set
+ * to 20px 0px 0px 20px.
+ */
 function switchLeft() {
 	switchBtnRight.classList.remove('active-case');
 	switchBtnLeft.classList.add('active-case');
@@ -12,6 +18,12 @@ function switchLeft() {
 	activeSwitch.style.borderRadius = '20px 0px 0px 20px';
 }
 
+/**
+ * When the switchRight function is called, the switchBtnRight class is added to the switchBtnRight
+ * element, the switchBtnLeft class is removed from the switchBtnLeft element, the left property of the
+ * activeSwitch element is set to 50%, and the borderRadius property of the activeSwitch element is set
+ * to 0px 20px 20px 0px
+ */
 function switchRight() {
 	switchBtnRight.classList.add('active-case');
 	switchBtnLeft.classList.remove('active-case');
@@ -19,10 +31,14 @@ function switchRight() {
 	activeSwitch.style.borderRadius = '0px 20px 20px 0px';
 }
 
+/* Adding an event listener to the switchBtnLeft element. When the element is clicked, the switchLeft
+function is called. */
 switchBtnLeft.addEventListener('click', function () {
 	switchLeft();
 }, false);
 
+/* Adding an event listener to the switchBtnRight element. When the element is clicked, the switchRight
+function is called. */
 switchBtnRight.addEventListener('click', function () {
 	switchRight();
 }, false);
