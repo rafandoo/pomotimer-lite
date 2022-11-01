1e35f835d2d64176bdf4e670f9e8edb653f11521
rafandoo <rafaelcamargo.inf@gmail.com>
2022-10-31 22:51:24 -0300

    Ciclos e opção de alarme

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,10 +1,17 @@
+/* Setting the default values for the local storage. */
+localStorage.setItem("focusMinutes", 25);
+localStorage.setItem("breakMinutes", 5);
+localStorage.setItem("cycles", 4);
+localStorage.setItem("status", "focus");
+localStorage.setItem("alarm", true);
+
 /* This is the JavaScript code that is used to make the buttons focus. */
 window.onload = function () {
-    var plusBtnBreak = document.getElementById("plusBtnBreak"),
-        minusBtnBreak = document.getElementById("minusBtnBreak"),
-        breakMinutes = document.getElementById("breakMinutes"),
+    var plusBtnBreak = document.querySelector("#plusBtnBreak"),
+        minusBtnBreak = document.querySelector("#minusBtnBreak"),
+        breakMinutes = document.querySelector("#breakMinutes"),
         breakValue = 5,
-        min = 0,
+        min = 1,
         max = 99;
 
     /* Saying that when the minus button is clicked, the value of the breakValue will decrease by 1. */
@@ -22,11 +29,11 @@ window.onload = function () {
         }
     }
 
-    var plusBtnFocus = document.getElementById("plusBtnFocus"),
-        minusBtnFocus = document.getElementById("minusBtnFocus"),
-        focusMinutes = document.getElementById("focusMinutes"),
+    var plusBtnFocus = document.querySelector("#plusBtnFocus"),
+        minusBtnFocus = document.querySelector("#minusBtnFocus"),
+        focusMinutes = document.querySelector("#focusMinutes"),
         focusValue = 25,
-        min = 0,
+        min = 1,
         max = 99;
 
     /* Saying that when the minus button is clicked, the value of the focusValue will decrease by 1. */
@@ -44,12 +51,37 @@ window.onload = function () {
         }
     }
 
+    var plusBtnCycles = document.querySelector("#plusBtnCycles"),
+        minusBtnCycles = document.querySelector("#minusBtnCycles"),
+        cycles = document.querySelector("#cycles"),
+        cyclesValue = 4,
+        min = 1,
+        max = 99;
+
+    /* Saying that when the minus button is clicked, the value of the cyclesValue will decrease by 1. */
+    minusBtnCycles.onclick = function () {
+        if (cyclesValue > min) {
+            cyclesValue -= 1;
+            cycles.innerText = cyclesValue;
+        }
+    }
+
+    /* Saying that when the plus button is clicked, the value of the cyclesValue will increase by 1. */
+    plusBtnCycles.onclick = function () {
+        if (cyclesValue < max) {
+            cyclesValue += 1;
+            cycles.innerText = cyclesValue;
+        }
+    }
+
     /* Saying that when the submit button is clicked, the value of the focusValue and breakValue will
     be stored in the local storage. */
     document.querySelector("form").addEventListener("submit", (e) => {
         e.preventDefault();
         localStorage.setItem("focusMinutes", focusMinutes.innerHTML);
         localStorage.setItem("breakMinutes", breakMinutes.innerHTML);
+        localStorage.setItem("cycles", cycles.innerHTML);
+        localStorage.setItem("alarm", document.querySelector("#alarmCheck").checked);
     });
 }
 
