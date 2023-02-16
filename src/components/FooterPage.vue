00e23f4c1fc9c9c3cecc73806f1f958e7470adfa
rafandoo <rafaelcamargo.inf@gmail.com>
2023-02-16 20:35:03 -0300

    PM-4 Add attribute rel noreferer

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -4,17 +4,17 @@
             <p class="text-muted mb-0">Copyright © 2023 {{ name }}</p>
             <ul class="list-inline mt-2">
                 <li class="list-inline-item me-4">
-                    <a :href="github_link" target="_blank">
+                    <a :href="github_link" target="_blank" rel='noopener noreferrer'>
                         <i class="fab fa-github text-light"></i>
                     </a>
                 </li>
                 <li class="list-inline-item me-4">
-                    <a :href="instagram_link" target="_blank">
+                    <a :href="instagram_link" target="_blank" rel='noopener noreferrer'>
                         <i class="fab fa-instagram text-light"></i>
                     </a>
                 </li>
                 <li class="list-inline-item">
-                    <a :href="personal_link" target="_blank">
+                    <a :href="personal_link" target="_blank" rel='noopener noreferrer'>
                         <i class="fas fa-code text-light"></i>
                     </a>
                 </li>
