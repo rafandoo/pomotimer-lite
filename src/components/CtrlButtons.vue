28dc12fe7759c5ad71d843fde13ca33daf5a420b
rafandoo <rafaelcamargo.inf@gmail.com>
2023-02-16 10:16:50 -0300

    PM-1 Refactor: Vue.JS

---
+++ src/components/CtrlButtons.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="row">
+        <div class="col text-center">
+            <button id="refreshBtn" class="text-white" type="button">
+                <i class="fa fa-refresh"></i>
+            </button>
+            <button id="playBtn" class="text-white actionBtn" type="button">
+                <i class="fas fa-play"></i>
+            </button>
+            <button id="pauseBtn" class="text-white actionBtn" type="button" hidden>
+                <i id="actIcon" class="fas fa-pause"></i>
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CtrlButtons'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ProgressTimer.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="row">
+        <div class="col">
+            <div class="text-white mt-7 mb-7">
+                <figure class="timer">
+                    <div class="minutes">0</div>
+                    <div>:</div>
+                    <div class="seconds">00</div>
+                    <audio :src="require('../assets/sounds/alarm.mp3')" id="alarm"></audio>
+                    <svg class="progress-ring" height="220" width="220">
+                        <circle class="progress-ring circle" stroke-width="4" fill="transparent" r="91" cx="110" cy="110" />
+                    </svg>
+                </figure>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProgressTimer'
+}
+</script>
+
+<style scoped>
+
+</style>
